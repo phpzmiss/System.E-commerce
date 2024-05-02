66bf05917ff303e52f29174163aaadb6603a2f92
phan.hong.phong <phong25092002@gmail.com>
2024-05-02 23:20:40 +0700

    Modify items.

--- vite-nijan/src/component/fragment/Item.jsx
+++ vite-nijan/src/component/fragment/Item.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import { FaHeartCirclePlus } from "react-icons/fa6";
-import { IoBagCheckOutline } from "react-icons/io5";
+import { FaCartArrowDown } from "react-icons/fa";
+import Button from "../admin/form/button/Button";
 
 const Item = ({ price, image, children, props }) => {
   return (
-    <div className="bg-[#ebebeb] flex flex-col my-5 cursor-pointer gap-y-2 p-3 rounded-md shadow-lg">
-      <div>
+    <div className="bg-[#ebebeb] flex flex-col items-center my-5 cursor-pointer gap-y-2 p-3 rounded-md shadow-lg">
+      <div className="w-full">
         <img
           className="w-full h-[250px] object-cover rounded-lg"
           src={image}
           alt=""
         />
       </div>
-      <div className="flex flex-col items-start justify-start ">
+      <div className="flex flex-col items-start justify-start w-full">
         <span>{children}</span>
         <div className="flex items-center justify-between w-full">
           <small>{price}vnđ</small>
           <div className="flex items-center gap-1 justify-centers">
-          <FaHeartCirclePlus className="text-xl transition-all hover:text-red-500" />
-          <div className="px-[10px] text-white rounded-full hover:bg-orange-700 transition-all py-[2px] flex justify-center items-center text-lg bg-orange-500">
-            +
-          </div>   
+          <FaHeartCirclePlus className="text-xl transition-all hover:text-red-500" />   
           </div>
         </div>
+
       </div>
+      <Button className="flex items-center justify-center px-4 py-2 mt-2 text-sm text-gray-400 transition-all rounded-sm bg-slate-300 hover:bg-slate-200">
+            Add to cart <span><FaCartArrowDown className="w-[24px] h-[24px] pl-2" /></span>
+      </Button>
     </div>
   );
 };
